4a6f97341d09cd7f06764c2cbee1d2ea666439b1
“amelia.khor23” <amelia.khor@xcitesolutions.com.au>
2024-12-05 16:41:59 +1100

    updated

--- src/components/BackdropEdit.js
+++ src/components/BackdropEdit.js
@@ -66,7 +66,6 @@ const BackdropEdit = ({ open, onClose, onSave, contact }) => {
               <p>{contact.company || "N/A"}</p>
               <TextField
                 label="Company"
-                variant="filled"
                 name="company"
                 value={formData.company}
                 onChange={handleChange}
@@ -79,7 +78,6 @@ const BackdropEdit = ({ open, onClose, onSave, contact }) => {
               <p>{contact.role || "N/A"}</p>
               <TextField
                 label="Role"
-                variant="filled"
                 name="role"
                 value={formData.role}
                 onChange={handleChange}
@@ -92,7 +90,6 @@ const BackdropEdit = ({ open, onClose, onSave, contact }) => {
               <p>{contact.firstName || "N/A"}</p>
               <TextField
                 label="First Name"
-                variant="filled"
                 name="firstName"
                 value={formData.firstName}
                 onChange={handleChange}
@@ -105,7 +102,6 @@ const BackdropEdit = ({ open, onClose, onSave, contact }) => {
               <p>{contact.lastName || "N/A"}</p>
               <TextField
                 label="Last Name"
-                variant="filled"
                 name="lastName"
                 value={formData.lastName}
                 onChange={handleChange}
@@ -118,7 +114,6 @@ const BackdropEdit = ({ open, onClose, onSave, contact }) => {
               <p>{contact.email || "N/A"}</p>
               <TextField
                 label="Email"
-                variant="filled"
                 name="email"
                 value={formData.email}
                 onChange={handleChange}
@@ -131,7 +126,6 @@ const BackdropEdit = ({ open, onClose, onSave, contact }) => {
               <p>{contact.phone || "N/A"}</p>
               <TextField
                 label="Phone"
-                variant="filled"
                 name="phone"
                 value={formData.phone}
                 onChange={handleChange}
@@ -143,11 +137,11 @@ const BackdropEdit = ({ open, onClose, onSave, contact }) => {
         )}
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleSave} color="primary">
-          Save
-        </Button>
         <Button onClick={onClose} color="primary">
           Cancel
+        </Button>
+        <Button onClick={handleSave} color="primary">
+          Save
         </Button>
       </DialogActions>
     </Dialog>
